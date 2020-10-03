dab9fcc3a3525df49d53d555dafd28e246332f43
DESKTOP-DK47FL5\Casa <felixpotente@gmail.com>
2020-10-03 21:01:35 +0200

    tutorial Completado

---
+++ res/resources.h
@@ -0,0 +1,11 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 sfx_laser[7680];
+extern const u8 sfx_explosion[20736];
+extern const SpriteDefinition ship;
+extern const SpriteDefinition bullet;
+extern const SpriteDefinition spr_powerup;
+extern const Image background;
+
+#endif // _RES_RESOURCES_H_
--- src/main.c
+++ src/main.c
@@ -1,16 +1,442 @@
-/**
- * Hello World Example
- * Created With Genesis-Code extension for Visual Studio Code
- * Use "Genesis Code: Compile" command to compile this program.
- **/
 #include <genesis.h>
+#include <resources.h>
+
+#define LEFT_EDGE 0
+#define RIGHT_EDGE 320
+#define BOTTOM_EDGE 224
+#define SHOT_INTERVAL 60
+#define SFX_LASER 64
+#define SFX_EXPLOSION 65
+
+typedef struct
+{
+	int x;
+	int y;
+	u16 w;
+	u16 h;
+	int velx;
+	int vely;
+	int health;
+	Sprite *sprite;
+	char name[6];
+} Entity;
+
+typedef enum
+{
+    RapidFire
+} Powerup;
+
+void shootBullet(Entity Shooter);
+
+#define MAX_BULLETS 6
+Entity bullets[MAX_BULLETS];
+u16 bulletsOnScreen = 0;
+
+#define MAX_PLAYER_BULLETS 3
+u16 maxPlayerBullets = MAX_PLAYER_BULLETS;
+u16 shotByPlayer = 0;
+
+#define MAX_ENEMIES 6
+Entity enemies[MAX_ENEMIES];
+u16 enemiesLeft = 0;
+
+#define POWERUP_DURATION 180
+u16 powerupTimer = 0;
+Entity powerup = {0, 0, 8, 8, 0, 0, 0, NULL};
+
+#define ANIM_STRAIGHT 0
+#define ANIM_MOVE 1
+Entity player = {0, 0, 16, 16, 0, 0, 0, NULL};
+
+int score = 0;
+char hud_string[40] = "";
+
+u16 shotTicker = 0;
+
+void updateScoreDisplay()
+{
+    sprintf(hud_string, "SCORE: %d - LEFT: %d", score, enemiesLeft);
+    VDP_clearText(0, 0, 40);
+    VDP_drawText(hud_string, 0, 0);
+}
+
+void myJoyHandler(u16 joy, u16 changed, u16 state)
+{
+	if (joy == JOY_1)
+	{
+		if (state & BUTTON_RIGHT)
+		{
+			player.velx = 2;
+			SPR_setAnim(player.sprite, ANIM_MOVE);
+			SPR_setHFlip(player.sprite, TRUE);
+		}
+		else if (state & BUTTON_LEFT)
+		{
+			player.velx = -2;
+			SPR_setAnim(player.sprite, ANIM_MOVE);
+			SPR_setHFlip(player.sprite, FALSE);
+		}
+		else
+		{
+			if ((changed & BUTTON_RIGHT) | (changed & BUTTON_LEFT))
+			{
+				player.velx = 0;
+				SPR_setAnim(player.sprite, ANIM_STRAIGHT);
+			}
+		}
+        if (state & BUTTON_B & changed)
+        {
+            shootBullet(player);
+        }
+        if (state & BUTTON_A & changed)
+        {
+            //activatePowerup(RapidFire);
+        }
+    }
+}
+
+void positionPlayer()
+{
+	/*Add the player's velocity to its position*/
+	player.x += player.velx;
+
+	/*Keep the player within the bounds of the screen*/
+	if (player.x < LEFT_EDGE)
+		player.x = LEFT_EDGE;
+	if (player.x + player.w > RIGHT_EDGE)
+		player.x = RIGHT_EDGE - player.w;
+
+	/*Let the Sprite engine position the sprite*/
+	SPR_setPosition(player.sprite, player.x, player.y);
+}
+
+void killEntity(Entity *e)
+{
+	e->health = 0;
+	SPR_setVisibility(e->sprite, HIDDEN);
+}
+
+void reviveEntity(Entity *e)
+{
+	e->health = 1;
+	SPR_setVisibility(e->sprite, VISIBLE);
+}
+
+void spawnPowerupAt(u16 X, u16 Y) {
+    powerup.x = X;
+    powerup.y = Y;
+    reviveEntity(&powerup);
+}
+
+void positionPowerup() {
+    if (powerup.health > 0)
+    {
+        powerup.y++;
+
+        if (powerup.y > BOTTOM_EDGE)
+        {
+            killEntity(&powerup);
+        }
+
+        if (collideEntities(&player, &powerup))
+        {
+            activatePowerup(RapidFire);
+            killEntity(&powerup);
+        }
+    }
+
+    SPR_setPosition(powerup.sprite, powerup.x, powerup.y);
+}
+
+void activatePowerup(Powerup Type) {
+    switch (Type)
+    {
+    case RapidFire:
+    {
+        maxPlayerBullets = MAX_BULLETS;
+        powerupTimer = POWERUP_DURATION;
+        VDP_setPaletteColor(18, RGB24_TO_VDPCOLOR(0xf8fc00));
+    }
+    default:
+        return;
+    }
+}
+
+void deactivatePowerup() {
+    maxPlayerBullets = MAX_PLAYER_BULLETS;
+    VDP_setPaletteColor(18, RGB24_TO_VDPCOLOR(0xf83800));
+}
+
+void positionEnemies()
+{
+    shotTicker++;
+
+	u16 i = 0;
+	for (i = 0; i < MAX_ENEMIES; i++)
+	{
+		Entity *e = &enemies[i];
+		if (e->health > 0)
+		{
+			/*Keep enemy within bounds*/
+			if ((e->x + e->w) > RIGHT_EDGE)
+			{
+				e->velx = -1;
+			}
+			else if (e->x < LEFT_EDGE)
+			{
+				e->velx = 1;
+			}
+
+			/*Apply velocity*/
+			e->x += e->velx;
+			SPR_setPosition(e->sprite, e->x, e->y);
+
+            /*Shoot*/
+            if(shotTicker > SHOT_INTERVAL){
+                if( (random() % (10-1+1)+1) > 4 ){
+                    shootBullet(*e);
+                    shotTicker = 0;
+                }
+            }
+		}
+	}
+}
+
+void shootBullet(Entity Shooter)
+{
+    bool fromPlayer = (Shooter.y > 100);
+
+    if (bulletsOnScreen < MAX_BULLETS)
+    {
+        if(fromPlayer == TRUE){
+            if (shotByPlayer >= maxPlayerBullets)
+            {
+                return;
+            }
+        }
+
+        Entity *b;
+        u16 i = 0;
+        for (i = 0; i < MAX_BULLETS; i++)
+        {
+            b = &bullets[i];
+            if (b->health == 0)
+            {
+                b->x = Shooter.x + 4;
+                b->y = Shooter.y;
+
+                reviveEntity(b);
+
+                if(fromPlayer == TRUE){
+                    b->vely = -3;
+                    shotByPlayer++;
+                } else{
+                    b->vely = 3;
+                }
+
+                SPR_setPosition(b->sprite, b->x, b->y);
+                bulletsOnScreen++;
+
+                XGM_startPlayPCM(SFX_LASER, 1, SOUND_PCM_CH2);
+
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+void positionBullets()
+{
+    u16 i = 0;
+    Entity *b;
+    for (i = 0; i < MAX_BULLETS; i++)
+    {
+        b = &bullets[i];
+        if (b->health > 0)
+        {
+            b->y += b->vely;
+
+            if (b->y + b->h < 0)
+            {
+                killEntity(b);
+                bulletsOnScreen--;
+                shotByPlayer--;
+            } else if(b->y > BOTTOM_EDGE){
+                killEntity(b);
+                bulletsOnScreen--;
+            }
+            else
+            {
+                SPR_setPosition(b->sprite, b->x, b->y);
+            }
+        }
+    }
+}
+
+int collideEntities(Entity* a, Entity* b)
+{
+    return (a->x < b->x + b->w && a->x + a->w > b->x && a->y < b->y + b->h && a->y + a->h >= b->y);
+}
+
+void handleCollisions()
+{
+    Entity *b;
+    Entity *e;
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < MAX_BULLETS; i++)
+    {
+        b = &bullets[i];
+        if (b->health > 0)
+        {
+            if(b->vely < 0){
+                for (j = 0; j < MAX_ENEMIES; j++)
+                {
+                    e = &enemies[j];
+                    if (e->health > 0)
+                    {
+                        if (collideEntities(b, e))
+                        {
+                            killEntity(e);
+                            killEntity(b);
+
+                            XGM_startPlayPCM(SFX_EXPLOSION, 1, SOUND_PCM_CH3);
+                            
+                            enemiesLeft--;
+                            bulletsOnScreen--;
+                            shotByPlayer--;
+
+                            score += 10;
+                            updateScoreDisplay();
+
+                            if (enemiesLeft % 5 == 0)
+                            {
+                                spawnPowerupAt(e->x, e->y);
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            } else{
+                if(collideEntities(b,&player)){
+                    killEntity(&player);
+                }
+            }            
+
+            
+        }
+    }
+}
 
 int main()
 {
-    VDP_drawText("Hello Sega!!", 10,13);
-    while(1)
+	JOY_init();
+	JOY_setEventHandler(&myJoyHandler);
+
+    XGM_setPCM(SFX_LASER, sfx_laser, sizeof(sfx_laser));
+    XGM_setPCM(SFX_EXPLOSION, sfx_explosion, sizeof(sfx_explosion));
+
+    SYS_disableInts();
+
+	VDP_loadTileSet(background.tileset,1,DMA);
+	VDP_setPalette(PAL1, background.palette->data);
+	VDP_setPalette(PAL2, background.palette->data);
+
+	int i = 0;
+	int thex = 0;
+	int they = 0;
+	int val = 1;
+
+	for( i=0; i < 1280; i++){
+		thex = i % 40;
+		they = i / 40;
+		val = (random() % (10 - 1 + 1)) + 1;
+		if (val > 3)
+			val = 1;
+		VDP_setTileMapXY(BG_B, TILE_ATTR_FULL(PAL1, 0, 0, 0, val), thex, they);
+	}
+
+	int offset = 0;
+	VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
+
+	SYS_enableInts();
+
+	SPR_init();
+
+	/*Add the player*/
+	player.x = 152;
+	player.y = 192;
+	player.health = 1;
+	player.sprite = SPR_addSprite(&ship, player.x, player.y, TILE_ATTR(PAL1, 0, FALSE, FALSE));
+
+	/*Create all enemy sprites*/
+	Entity *e = enemies;
+	for (i = 0; i < MAX_ENEMIES; i++)
+	{
+		e->x = i * 32;
+		e->y = 32;
+		e->w = 16;
+		e->h = 16;
+		e->velx = 1;
+		e->health = 1;
+		e->sprite = SPR_addSprite(&ship, e->x, e->y, TILE_ATTR(PAL2, 0, TRUE, FALSE));
+		sprintf(e->name, "En%d", i);
+		enemiesLeft++;
+		e++;
+	}
+
+    /*Create all bullet sprites*/
+    Entity *b = bullets;
+    for (i = 0; i < MAX_BULLETS; i++)
     {
-        VDP_waitVSync();
+        b->x = 0;
+        b->y = -10;
+        b->w = 8;
+        b->h = 8;
+        b->sprite = SPR_addSprite(&bullet, bullets[0].x, bullets[0].y, TILE_ATTR(PAL1, 0, FALSE, FALSE));
+        sprintf(b->name, "Bu%d", i);
+        b++;
     }
-    return (0);
+
+    /*Create the powerup*/
+    powerup.sprite = SPR_addSprite(&spr_powerup, powerup.x, powerup.y, TILE_ATTR(PAL1, 0, FALSE, FALSE));
+    killEntity(&powerup);
+
+    updateScoreDisplay();
+
+    VDP_setPaletteColor(34, RGB24_TO_VDPCOLOR(0x0078f8));
+
+	SPR_update();
+
+	while(1)
+	{
+		VDP_setVerticalScroll(BG_B, offset -= 2);
+		if (offset >= 256)
+			offset = 0;
+
+		positionPlayer();
+		positionEnemies();
+        positionBullets();
+        positionPowerup();
+        handleCollisions();
+
+        if (powerupTimer > 0)
+        {
+            powerupTimer--;
+            if (powerupTimer == 0)
+            {
+                deactivatePowerup();
+            }
+        }
+
+        SPR_update();
+        VDP_waitVSync();
+	}
+
+	//Stop doing cool stuff :(
+	return(0);
 }
